2e7968212654a421ccb6fcef6284e0727ee79382
Adrián Acosa Sánchez <adrianacosasanchez@proton.me>
2026-02-27 16:38:29 +0100

    add opencode and antigravity configs

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -17,3 +17,6 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+# opencode
+export PATH=/home/adacosdev/.opencode/bin:$PATH
